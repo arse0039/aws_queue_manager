c500d47bf6d60cb78d0a32ec9065cd92e9f72bd4
Brandon Arsenault <arse0039@gmail.com>
2024-06-25 13:20:32 -0700

    Updated to use next.js. Added basic routes

---
+++ src/app/TALanding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TALandingPage = () => {
+    return (
+        <div>
+            This is the main page TA's will see when they log in.
+        </div>
+    )
+}
+
+export default TALandingPage;
